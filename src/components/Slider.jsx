a343afd2a24eb566e366a86ce15d3f1813204b5a
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-03-09 10:10:56 +0600

    slider added

---
+++ src/components/Slider.jsx
@@ -0,0 +1,21 @@
+const data = [
+  {
+    id: 1,
+    src: '',
+    alt: '',
+  },
+];
+
+const Slider = () => {
+  return (
+    <div className="slider">
+      <div className="slide"></div>
+      <div className="slide"></div>
+      <div className="slide"></div>
+      <div className="slide"></div>
+      <div className="slide"></div>
+    </div>
+  );
+};
+
+export default Slider;
